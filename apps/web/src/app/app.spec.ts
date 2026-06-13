5b225eac71687610a70c1387ca374a3a78a383c9
olafvanv <olafvanv@gmail.com>
2026-06-13 21:23:10 +0200

    refactor: improve type safety in app tests and adjust shell component styling and layout constants

--- apps/web/src/app/app.spec.ts
+++ apps/web/src/app/app.spec.ts
@@ -1,16 +1,16 @@
 import { TestBed } from '@angular/core/testing';
-import { App } from './app';
-import { SettingsStateService } from '@expose/data-access';
 import { RouterModule } from '@angular/router';
+import { SettingsStateService } from '@expose/data-access';
+import { App } from './app';
 
 describe('App', () => {
-  let settingsStateMock: any;
+  let settingsStateMock: SettingsStateService;
 
   beforeEach(async () => {
     settingsStateMock = {
       theme: () => 'system',
       preferredHomePage: () => 'sessions',
-    };
+    } as SettingsStateService;
 
     await TestBed.configureTestingModule({
       imports: [App, RouterModule.forRoot([])],
